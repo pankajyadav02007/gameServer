5091534177396c72917749dac11a2d4061dc06df
pankaj <pankajyadav221306@gmail.com>
2025-10-13 07:04:45 +0530

    code added

--- index.mjs
+++ index.mjs
@@ -1,28 +1,31 @@
-// import dotenv from 'dotenv'
-// dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 
-import express from 'express'
-import { errorHandler } from './error.mjs'
-import userRouter from './users/router.mjs'
-import gameRouter from './game/router.mjs'
-import sessionRouter from './session/router.mjs'
-const app = express()
+import express from "express";
+import cors from "cors";
+import { errorHandler } from "./error.mjs";
+import userRouter from "./users/router.mjs";
+import gameRouter from "./game/router.mjs";
+import sessionRouter from "./session/router.mjs";
+const app = express();
 
-app.use(express.json())
+app.use(cors());
 
-app.use('/users', userRouter)
-app.use('/games', gameRouter)
-app.use('/sessions', sessionRouter)
+app.use(express.json()); // without this middlware req.body will be undefined
+
+app.use("/users", userRouter);
+app.use("/games", gameRouter);
+app.use("/sessions", sessionRouter);
 
 app.all(/^.*$/, (req, res) => {
-  res.status(400).json({ msg: 'route dosen\'t exists' })
-})
+  res.status(400).json({ msg: "route dosen't exists" });
+});
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.listen(process.env.PORT, (err) => {
   if (err) {
-    console.log(err)
+    console.log(err);
   }
-  console.log(`Server started on port ${process.env.PORT}`)
-})
+  console.log(`Server started on port ${process.env.PORT}`);
+});
